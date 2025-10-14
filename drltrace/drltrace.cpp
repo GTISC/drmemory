cd88fbe38457299cffb442cd662219bac0d2f2cb
James-yaoshenglong <yaoshenglong758063348@gmail.com>
2025-10-14 20:49:24 +0000

    revert back to pre debugging

--- drltrace/drltrace.cpp
+++ drltrace/drltrace.cpp
@@ -230,61 +230,74 @@ print_symbolic_args(const char *name, void *wrapcxt, app_pc func)
     }
 }
 
+/****************************************************************************
+ * Library exit wrapping
+ */
+
 static void
 lib_exit(void *wrapcxt, void *user_data)
 {
     const char *name = (const char *) user_data;
-    // blacklist some apis that may lead to crash
+    ptr_uint_t retval = (ptr_uint_t)drwrap_get_retval(wrapcxt);
     void *drcontext = drwrap_get_drcontext(wrapcxt);
-    module_data_t *mod;
-    app_pc retaddr =  NULL;
-    if (wrapcxt == NULL || name == NULL)
-        return;
-    dr_fprintf(outf, "start filtering weird api %s\n", name);
-    DR_TRY_EXCEPT(drcontext, {
-        retaddr = drwrap_get_retaddr(wrapcxt);
-    }, { /* EXCEPT */
-        retaddr = NULL;
-        dr_fprintf(outf, "<invalid memory>");
-    });
-    if (retaddr != NULL) {
-        mod = dr_lookup_module(retaddr);
-        dr_fprintf(outf, "got module data\n");
-        // if (mod != NULL) {
-        //     bool from_exe = (mod->start == exe_start);
-        //     dr_free_module_data(mod);
-        //     if (!from_exe)
-        //         return;
-        // }
-    } else {
-        dr_fprintf(outf, "could not get return address\n");
-        /* Nearly all of these cases should be things like KiUserCallbackDispatcher
-            * or other abnormal transitions.
-            * If the user really wants to see everything they can not pass
-            * -only_from_app.
-            */
-        return;
-    }
-
-    if (strcmp(name, "RtlFreeHeap") == 0) {
-        dr_fprintf(outf, "%s returned\n", name);
-        return;
-    }
-    dr_fprintf(outf, "Function %s exit\n", name);
-    dr_fprintf(outf, "start get return value\n");
-    ptr_uint_t retval =  NULL;
-    DR_TRY_EXCEPT(drcontext, {
-        retval = (ptr_uint_t)drwrap_get_retval(wrapcxt);
-    }, { /* EXCEPT */
-        retval = NULL;
-    });
-    dr_fprintf(outf, "got return value\n");
-    dr_fprintf(outf, "start get thread id\n");
     thread_id_t tid = dr_get_thread_id(drcontext);
-    dr_fprintf(outf, "got thread id\n");
 
-    // dr_fprintf(outf, "print return value: %d", retval);
-    dr_fprintf(outf, "%s returned\n", name);
+    // Get module information for the function
+    const char *modname = NULL;
+    app_pc func = drwrap_get_func(wrapcxt);
+    module_data_t *mod = dr_lookup_module(func);
+    if (mod != NULL)
+        modname = dr_module_preferred_name(mod);
+
+    // Try to get return value information from config if available
+    drsys_arg_t *ret_arg_config = NULL;
+    if (op_use_config.get_value()) {
+        ret_arg_config = return_value_search(name);
+    }
+
+    // Create argument structure for the return value
+    drsys_arg_t ret_arg;
+    memset(&ret_arg, 0, sizeof(ret_arg));
+    ret_arg.value = retval;
+    ret_arg.value64 = (uint64)retval;
+    ret_arg.ordinal = -1;  // Special ordinal for return value
+    ret_arg.mode = DRSYS_PARAM_RETVAL;
+    ret_arg.pre = false;
+    ret_arg.size = sizeof(ptr_uint_t);
+    ret_arg.reg = DR_REG_NULL;
+
+    // Use config information if available, otherwise use defaults
+    if (ret_arg_config != NULL) {
+        ret_arg.type = ret_arg_config->type;
+        ret_arg.type_name = ret_arg_config->type_name;
+        ret_arg.arg_name = ret_arg_config->arg_name;
+        if (ret_arg_config->size > 0)
+            ret_arg.size = ret_arg_config->size;
+    } else {
+        // Default fallback
+        ret_arg.type = DRSYS_TYPE_VOID;
+        ret_arg.type_name = "void";
+        ret_arg.arg_name = "retval";
+    }
+
+    // Print thread ID and module!function name
+    if (tid != INVALID_THREAD_ID)
+        dr_fprintf(outf, "~~%d~~ ", tid);
+    else
+        dr_fprintf(outf, "~~Dr.L~~ ");
+
+    // Print module!function name
+    dr_fprintf(outf, "%s%s%s", modname == NULL ? "" : modname,
+            modname == NULL ? "" : "!", name);
+
+    // Print the return value using existing print_arg function
+    print_arg(drcontext, &ret_arg);
+
+    dr_fprintf(outf, "\n");
+
+    // Clean up module data
+    if (mod != NULL)
+        dr_free_module_data(mod);
 }
 
 /****************************************************************************
